e183e946fc79775c470139d3c3480bbf666d1c1d
venkat_vandy <venkataramana.nagarajan@vanderbilt.edu>
2017-04-27 15:42:38 -0500

    Raft Leader Election

---
+++ Raft/message_types.py
@@ -0,0 +1,20 @@
+#Message Types
+
+class ControlMessageTypes():
+    JOIN_NETWORK = 0
+    SYNC_NETWORK = 1
+    NODE_DROP = 2
+    LEADER_PROPOSE = 3
+    LEADER_NO = 4
+    LEADER_GO = 5
+    SEND_VAL = 6
+    VAL_ACCEPTED = 7
+    ASK_FOR_VOTE = 8
+    I_AM_LEADER = 9
+
+class MessageTypes():
+    MSG_ACK = 10
+    PING = 11
+    I_VOTE_FOR_YOU = 12
+    I_DO_NOT_VOTE_FOR_YOU = 13
+    
