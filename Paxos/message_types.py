a366b2744a87f45284b12eeef8d2da66ce935976
venkat_vandy <venkataramana.nagarajan@vanderbilt.edu>
2017-05-03 20:48:25 -0500

    Paxos

---
+++ Paxos/message_types.py
@@ -0,0 +1,16 @@
+#Message Types
+
+class ControlMessageTypes():
+    JOIN_NETWORK = 0
+    SYNC_NETWORK = 1
+    NODE_DROP = 2
+    LEADER_PROPOSE = 3
+    LEADER_NO = 4
+    LEADER_GO = 5
+    SEND_VAL = 6
+    VAL_ACCEPTED = 7
+
+class MessageTypes():
+    MSG_ACK = 8
+    PING = 9
+    
